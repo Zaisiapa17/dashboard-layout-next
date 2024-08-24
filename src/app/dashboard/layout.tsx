5147fef00416e6a12e20a3eaa87ce1f18fd1b693
alie <mushyafaali@gmail.com>
2024-08-24 22:10:07 +0700

    tidy up the code

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,18 +16,18 @@ export default function DashboardLayout({
 }) {
 
     const [openSidebar, setOpenSidebar] = useState<boolean>(true)
-    
+
     return (
-        <div className="min-h-screen">
-        <nav className="bg-lime-400 hidden lg:inline float-left w-1/5 h-screen">
-            navbar
-        </nav>
-        <header className="bg-orange-500 block w-full h-[6vh] lg:h-[9vh] lg:w-4/5 lg:ml-[20%]">
-            header
-        </header>
-        <main className="bg-yellow-400 block h-[94vh] lg:h-[91vh] lg:w-4/5 lg:ml-[20%]">
-            {children}
-        </main>
-        </div>
+        <>
+            <nav className="bg-lime-400 hidden lg:inline float-left w-1/5 h-screen">
+                navbar
+            </nav>
+            <header className="bg-orange-500 block w-full h-[6vh] lg:h-[9vh] lg:w-4/5 lg:ml-[20%]">
+                header
+            </header>
+            <main className="bg-yellow-400 block h-[94vh] lg:h-[91vh] lg:w-4/5 lg:ml-[20%]">
+                {children}
+            </main>
+        </>
     )
 }
