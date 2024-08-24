d24a59d6e9d5ba5d92767e3eb841580df7913283
alie <mushyafaali@gmail.com>
2024-08-24 21:29:32 +0700

    repalcing

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -18,61 +18,16 @@ export default function DashboardLayout({
     const [openSidebar, setOpenSidebar] = useState<boolean>(true)
     
     return (
-        <section className="grid grid-rows-12 grid-cols-16 w-screen h-screen">
-            <header className={`bg-red-400 row-start-1 row-end-2 col-start-1 md:col-start-1 ${openSidebar? 'lg:col-start-4' : 'lg:col-start-2'} col-end-17`}>
-                <div className="flex h-full items-center gap-4 border-b bg-muted/40 px-4 lg:px-6">
-                    {/* <NavbarMobile
-                        activeItem={activeItem}
-                        role={userRole}
-                        onSetActiveItem={handleSetActiveItem}
-                    /> */}
-                    <Button
-                        variant="outline"
-                        size="icon"
-                        className="shrink-0 hidden lg:flex"
-                        onClick={() => setOpenSidebar(!openSidebar)}
-                    >
-                        <FiMenu size={20} />
-                        <span className="sr-only">Toggle navigation menu</span>
-                    </Button>
-                    <div className="w-full flex-1">
-                        <form>
-                            <div className="relative">
-                                <IoSearch className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                                <Input
-                                    type="search"
-                                    placeholder="Search products..."
-                                    className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-                                />
-                            </div>
-                        </form>
-                    </div>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="secondary" size="icon" className="rounded-full">
-                                <LuUserCircle size={20} />
-                                <span className="sr-only">Toggle user menu</span>
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>Settings</DropdownMenuItem>
-                            <DropdownMenuItem>Support</DropdownMenuItem>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>Logout</DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </div>
-            </header>
-            <nav className={`bg-yellow-200 hidden lg:inline-block lg:row-start-1 lg:row-end-13 lg:col-start-1 ${openSidebar? 'lg:col-end-4' : 'lg:col-end-2'}`}>
-                navbar
-            </nav>
-            <main className={`bg-slate-500 row-start-2 row-end-13 col-start-1 md:col-start-1 ${openSidebar? 'lg:col-start-4' : 'lg:col-start-2'} col-end-17`}>
-                <ScrollArea className="h-full w-full">
-                    {children}
-                </ScrollArea>
-            </main>
-        </section>
+        <>
+        <nav>
+            navbar
+        </nav>
+        <header>
+            header
+        </header>
+        <main>
+            {children}
+        </main>
+        </>
     )
 }
