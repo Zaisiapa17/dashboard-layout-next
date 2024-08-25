6445d6948f00da56298089fb3d45ee014abe4b19
alie <mushyafaali@gmail.com>
2024-08-25 12:07:35 +0700

    add scrollbar navbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -20,9 +20,27 @@ export default function DashboardLayout({
     return (
         <div className="w-screen">
             <nav className={`bg-lime-400 hidden lg:inline float-left ${openSidebar ? "w-[20vw]" : "w-[6vw]"} w-1/5 h-screen transition-all duration-300`}>
-                navbar
+                <div className="flex flex-col items-center h-screen">
+                    <div className="bg-indigo-500 flex items-center justify-center w-full h-[60px] border-b">
+                        <h1 className={`transition-all duration-300 ${openSidebar ? "opacity-100 scale-100" : "opacity-0 scale-0"}`}>
+                            Webzet.dev
+                        </h1>
+                    </div>
+                    <div className="bg-slate-400 w-10/12 h-4/5">
+                        <ScrollArea className="h-full">
+                            <div className="h-[1000px]">
+                                main nav
+                            </div>
+                        </ScrollArea>
+                    </div>
+                    <div className="bg-indigo-500 flex items-center justify-center w-full h-[60px] border-t mt-auto">
+                        <h1 className={`transition-all duration-300 ${openSidebar ? "opacity-100 scale-100" : "opacity-0 scale-0"}`}>
+                            Webzet.dev
+                        </h1>
+                    </div>
+                </div>
             </nav>
-            <header className="bg-orange-500 block h-[6vh] lg:h-[9vh] lg:w-screen transition-all duration-300">
+            <header className="bg-orange-500 block h-[6vh] lg:h-[60px] lg:w-screen transition-all duration-300">
                 <div className="flex h-full items-center justify-between gap-4 border-b bg-muted/40 px-4 lg:px-6">
                     <Button
                         variant="outline"
@@ -51,7 +69,7 @@ export default function DashboardLayout({
                     </DropdownMenu>
                 </div>
             </header>
-            <main className="bg-yellow-400 h-[calc(100vh-6vh)] lg:h-[calc(100vh-9vh)] transition-all duration-300">
+            <main className="bg-yellow-400 h-[calc(100vh-6vh)] lg:h-[calc(100vh-60px)] transition-all duration-300">
                 <ScrollArea className="h-full">
                     {children}
                 </ScrollArea>
